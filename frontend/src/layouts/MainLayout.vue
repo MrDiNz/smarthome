8e0dae3616177a9a03fc80573d91421c0b4d0d5c
Mr.DiNz <poo.hu@hotmail.com>
2020-10-07 01:18:49 +0700

    doing

---
+++ frontend/src/layouts/MainLayout.vue
@@ -0,0 +1,60 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          aria-label="Menu"
+          dense
+          flat
+          icon="menu"
+          round
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+
+        <q-toolbar-title>
+          Quasar App
+        </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      bordered
+      content-class="bg-grey-1"
+      show-if-above
+    >
+      <q-list>
+        <q-item-label class="text-grey-8" header>
+          Menu
+        </q-item-label>
+        <q-item v-for="link in links" :key="link.path">
+          <q-item-section v-if="link.icon" avatar>
+            <q-icon :name="link.icon" />
+          </q-item-section>
+
+          <q-item-section>
+            <q-item-label>{{ link.name }}</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component({
+  components: {}
+})
+export default class MainLayout extends Vue {
+  leftDrawerOpen = false;
+  links = [{ path: '/', icon: 'fas fa-home', name: 'Home' }];
+}
+</script>
